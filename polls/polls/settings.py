3dc7bb64d306d9aaa932d71e0667122fbfdf6d73
lesmadeit <lesmwendwa@gmail.com>
2024-12-18 14:37:58 +0300

    changes

--- polls/polls/settings.py
+++ polls/polls/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pollster.apps.PollsterConfig',
+    'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- polls/polls/urls.py
+++ polls/polls/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('pages.urls')),
     path('pollster/', include('pollster.urls')),
     
 ]
